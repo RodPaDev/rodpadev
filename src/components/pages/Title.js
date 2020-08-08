5f3cc2da01843fef192ec299ee409687cddbb499
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-08-08 16:13:58 +0100

    add stylings

--- src/components/pages/Title.js
+++ src/components/pages/Title.js
@@ -1,13 +1,28 @@
 import React from "react";
+import styled from 'styled-components';
+
+const Header = styled.header`
+    h1{
+        font-size: 4rem;
+        font-weight: 600;
+        span{
+            color: #FF5C00;
+        }
+    }
+    h2{
+        font-size: 2.4rem;
+        font-weight: 300;
+    }
+`;
 
 function Title() {
   return (
-    <div>
+    <Header>
       <h1>
         rodpa<span>dev</span>
       </h1>
       <h2>software developer</h2>
-    </div>
+    </Header>
   );
 }
 
