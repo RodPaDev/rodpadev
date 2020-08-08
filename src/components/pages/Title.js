2ce69d55881a83e361a100b2a0229f7ab9d67c65
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-08-08 16:03:58 +0100

    add components

---
+++ src/components/pages/Title.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div>
+      <h1>
+        rodpa<span>dev</span>
+      </h1>
+      <h2>software developer</h2>
+    </div>
+  );
+}
+
+export default Title;
