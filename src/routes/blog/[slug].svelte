eb520074e619c99847027f651001e29affae7477
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2020-11-06 14:35:28 +0000

    Develop (#3)
    * wip
    
    * add projects
    
    * add projects
    
    * add og meta
    
    * add time sort and display to projects
    
    * add og
    
    * remove console.log

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -137,6 +137,7 @@
   <meta property="og:title" content="{data.title}">
   <meta property="og:image" content="{data.featured_image}">
   <meta property="og:type" content="article" />
+  <meta property="og:url" content="{data.url}" />
 </svelte:head>
 
 <article>
